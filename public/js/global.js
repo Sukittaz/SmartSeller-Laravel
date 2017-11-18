dd2603640892ea9dc3ebd8cf34cbee4c2c39827b
Sukittaz <willian_pg@hotmail.com>
2017-11-18 17:20:27 -0200

    Dellete old controller

--- public/js/global.js
+++ public/js/global.js
@@ -10,3 +10,24 @@ $(function () {
 	  'autoWidth'   : false
 	})
 })
+
+/* Search CEP CostumerADD */
+$('input[name=CostumerCEP]').on('blur', function(){
+	var cep = $(this).val();
+
+	$.ajax({
+		url:'http://api.postmon.com.br/v1/cep/'+cep,
+		type:'GET',
+		dataType:'json',
+		success:function(json){
+			if(typeof json.logradouro != 'undefined') {
+				$('input[name=CostumerAddres]').val(json.logradouro);
+				$('input[name=CostumerNeigh]').val(json.bairro);
+				$('input[name=CostumerUF]').val(json.estado);
+				$('input[name=CostumerCity]').val(json.cidade);
+				$('input[name=CostumerCountry]').val('Brasil');
+				$('input[name=CostumerAddresNumber]').focus();
+			}
+		}
+	});
+});
