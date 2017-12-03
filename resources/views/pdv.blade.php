a45325cce99057fcb4974f464171878b6c047ea7
Sukittaz <willian_pg@hotmail.com>
2017-12-03 17:24:50 -0200

    Frente de caixa view Parcial

--- resources/views/pdv.blade.php
+++ resources/views/pdv.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>AdminLTE Dashboard</title>
+    <title>Frente de Caixa</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.2 -->
     <link href="http://smartseller.dev/css/bootstrap.css" rel="stylesheet" type="text/css">
@@ -11,34 +11,198 @@
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css">
     <!-- Theme style -->
     <link href="http://smartseller.dev/css/adminlte-app.css" rel="stylesheet" type="text/css">
-    <!-- Global JS -->
-    <link href="http://smartseller.dev/css/global.css" rel="stylesheet" type="text/css">
-
+    <!-- PDV CSS -->
+    <link href="http://smartseller.dev/css/pdv.css" rel="stylesheet" type="text/css">
     <link href="http://smartseller.dev/css/skins/skin-blue.css" rel="stylesheet" type="text/css">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="http://smartseller.dev/plugins/dataTables.bootstrap.css">    
-    <!-- Select2 -->
-    <link rel="stylesheet" href="http://smartseller.dev/plugins/select2.min.css">
 </head>
-	<body class="skin-blue" >
-		<header class="main-header">
-		    <!-- Logo -->
-		    <a href="index2.html" class="logo"><b>Admin</b>LTE</a>
-		    <!-- Header Navbar -->
-		    <nav class="navbar navbar-static-top" role="navigation">
-		        <div class="navbar-custom-menu">
-		            <ul class="nav navbar-nav">
-		                <li class="dropdown user user-menu">
-		                    <a href="login/logout/">
-		                        <span class="hidden-xs">Sair do sistema</span>
-		                    </a>
-		                </li>
-		            </ul>
-		        </div>
-		    </nav>
-		</header>
-		<div>
-			teste
+<body class="skin-blue" >
+	<header class="main-header">
+	    <!-- Logo -->
+	    <a href="index2.html" class="logo"><b>Admin</b>LTE</a>
+	    <!-- Header Navbar -->
+	    <nav class="navbar navbar-static-top" role="navigation">
+	        <div class="navbar-custom-menu">
+	            <ul class="nav navbar-nav">
+	                <li class="dropdown user user-menu">
+	                    <a href="login/logout/">
+	                        <span class="hidden-xs">Sair do sistema</span>
+	                    </a>
+	                </li>
+	            </ul>
+	        </div>
+	    </nav>
+	</header>
+	<div class="container-fluid">
+		<div class="row row-one text-center line-space">
+		  	<div class="col-md-3">
+		  		<p>Vendedor(F11)</p>
+		  	</div>		  	
+		  	<div class="col-md-3">
+		  		<p>Cliente(F545)</p>
+		  	</div>		  	
+		  	<div class="col-md-3">
+		  		<p>Cliente(F545)</p>
+		  	</div>
+		  	<div class="col-md-3">
+		  		<p>Cliente(F545)</p>
+		  	</div>		  	
 		</div>
-	</body>
+		<div class="row row-two line-space">
+			COCA COLA
+		</div>	
+		<div class="row row-three line-space-min">
+		  	<div class="col-md-4">
+			    <div class="form-group">
+			      <label>Nome:</label>
+			      <input type="text" name="ProductCode" class="form-control">
+			    </div>
+		  		<div class="row">
+				    <div class="form-group">
+					    <div class="col-md-3">
+					    	<label>Nome:</label>
+					    </div>
+					    <div class="col-md-6">
+					    	<input type="text" name="ProductCode" class="form-control">
+	       				</div>
+				    </div>		  			
+		  		</div>
+		  		<div class="row line-space-min">
+				    <div class="form-group">
+					    <div class="col-md-3">
+					    	<label>Nome:</label>
+					    </div>
+					    <div class="col-md-6">
+					    	<input type="text" name="ProductCode" class="form-control">
+	       				</div>
+				    </div>		  			
+		  		</div>
+		  		<div class="row line-space-min">
+				    <div class="form-group">
+					    <div class="col-md-3">
+					    	<label>Nome:</label>
+					    </div>
+					    <div class="col-md-6">
+					    	<input type="text" name="ProductCode" class="form-control">
+	       				</div>
+				    </div>		  			
+		  		</div>
+		  		<div class="row line-space-min">
+				    <div class="form-group">
+					    <div class="col-md-3">
+					    	<label>Nome:</label>
+					    </div>
+					    <div class="col-md-6">
+					    	<input type="text" name="ProductCode" class="form-control">
+	       				</div>
+				    </div>		  			
+		  		</div>
+		  		<div class="col-md-9">
+		  			<div class="row shortcuts line-space-min">
+		  				<ul>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  					<li>(F11) Finalizar Venda</li>
+		  				</ul>
+		  			</div>
+		  		</div>		  				  				    			    			    
+			</div>
+			<div class="col-md-8">
+				<div class="row products-list">
+
+					<div class="col-md-12">
+						<div class="row linetop">
+							<div class="col-md-6 text-left">dasdsa</div>
+							<div class="col-md-6 text-right">dasdsads</div>
+
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<table class="table table-sm products-list-table">
+									<thead>
+								      <tr>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								        <th>Nome</th>
+								      </tr>							
+									</thead>
+								      <tbody>
+									      <tr>
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									      </tr>
+									      <tr>
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									      </tr>
+									      <tr>
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									      </tr>
+									      <tr>
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+									        <td>Coca-Cola</td>   
+								      </tbody>						
+								</table>	
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 line-space-max">
+					<div class="row">
+						<div class="col-md-4 col-md-offset-8">
+							<div class="row text-center">
+								<div class="col-md-12 sale-price">
+									<p>R$: 950,00</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- PDV JS -->
+	<script src="{{ asset ("/js/pdv.js") }}" type="text/javascript"></script>	
+</body>
 </html>
