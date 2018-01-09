d2416a92cd489a58cf70045a2d72487bced8f54f
Sukittaz <willian_pg@hotmail.com>
2018-01-08 22:48:36 -0200

    pdvController

--- app/Http/Controllers/PdvController.php
+++ app/Http/Controllers/PdvController.php
@@ -3,22 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Base\User;
+use App\Repositories\UserRepository;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 
 class PdvController extends Controller {
 
-	private $CompanyID;
+    private $CompanyID;
 
-	public function __construct(Redirector $redirect) {
-		$this->CompanyID = session()->get('user')['CompanyID'];
-
-		$user = new User;
-        if($user->isLogged() == false) {
-			$redirect->to('login')->send();
+    public function __construct(Redirector $redirect) {
+        $this->CompanyID = session()->get('user')['CompanyID'];
+        
+        if(UserRepository::isLogged() == false) {
+            $redirect->to('login')->send();
         }
-	}	
+    }
 
     public function index() {
         $user  = User::with('bunch')->where('CompanyID', '=', $this->CompanyID)->get();
